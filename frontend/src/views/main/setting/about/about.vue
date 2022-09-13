6b795c48f7ff43fbae5a53e8b8cfa3b12e51f483
zy7y <13271962515@163.com>
2022-09-13 21:59:01 +0800

    feat: 动态路由

---
+++ frontend/src/views/main/setting/about/about.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>about</div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/views/main/system/menu/menu.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>menu</div>
+</template>
+
+<style scoped></style>
