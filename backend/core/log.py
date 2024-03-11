721229e7aeba7e2bd705092f85687535839406d4
1024小神 <15670339118@163.com>
2024-03-11 16:35:28 +0800

    fix:Logging error UnicodeEncodeError: 'gbk' codec can't encode character '\xae' in position 31: illegal multibyte sequence

--- backend/core/log.py
+++ backend/core/log.py
@@ -4,7 +4,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # 创建一个 FileHandler，用于写入日志文件
-handler = logging.FileHandler('log_file.log')
+handler = logging.FileHandler('log_file.log', encoding="utf-8")
 handler.setLevel(logging.INFO)
 
 # 设置日志格式
